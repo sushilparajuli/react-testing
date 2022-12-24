fdb232752646f49b67ad2e07a281038f590fa2c3
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-12-24 23:29:02 +0400

    queryByRole implement with info

--- src/components/skills/skills.test.tsx
+++ src/components/skills/skills.test.tsx
@@ -8,4 +8,12 @@ describe("Skills", () => {
     const listItemElements = screen.getAllByRole("listitem");
     expect(listItemElements).toHaveLength(skills.length); // as we passed 3 skills
   });
+
+  it("testing queryByRole", () => {
+    render(<Skills skills={skills} />);
+    const startLearningButton = screen.queryByRole("button", {
+      name: "Start learning",
+    }); // return matching node or null if no elements, useful for asserting elements which are not present, throws error if more than one found
+    expect(startLearningButton).not.toBeInTheDocument(); // as we passed 3 skills
+  });
 });
