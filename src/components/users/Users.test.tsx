3475ac263fe5b11cdd7d8f3b1a87227c101ed3b1
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-12-25 16:05:35 +0400

    error case by using server.use for specific test
    - making error api req in isolation without modifying main mock handlers .

--- src/components/users/Users.test.tsx
+++ src/components/users/Users.test.tsx
@@ -1,5 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import { Users } from "./Users";
+import { server } from "../../mocks/server";
+import { rest } from "msw";
 
 describe("Users", () => {
   test("renders correctly", () => {
@@ -12,5 +14,19 @@ describe("Users", () => {
     render(<Users />);
     const users = await screen.findAllByRole("listitem");
     expect(users).toHaveLength(3);
+  });
+
+  test("renders error", async () => {
+    server.use(
+      rest.get(
+        "https://jsonplaceholder.typicode.com/users",
+        (req, res, ctx) => {
+          return res(ctx.status(500));
+        }
+      )
+    );
+    render(<Users />);
+    const error = await screen.findByText("Error fetching users");
+    expect(error).toBeInTheDocument();
   });
 });
