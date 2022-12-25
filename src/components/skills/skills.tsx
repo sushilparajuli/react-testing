bd1deb3a3418f874879ff449bf1f9dfacbca367c
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-12-25 10:40:24 +0400

    findBy example with timeout config

--- src/components/skills/skills.tsx
+++ src/components/skills/skills.tsx
@@ -1,8 +1,13 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { SkillsProps } from "./skills.types";
 
 export default function Skills({ skills }: SkillsProps) {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoggedIn(true);
+    }, 1001);
+  }, []);
   return (
     <>
       <ul>
