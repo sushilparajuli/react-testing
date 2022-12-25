777950a349c47785ae5ac2d390542603a9ae2953
Sushil Parajuli <sushilparajuli2010@gmail.com>
2022-12-25 15:28:07 +0400

    mocking method passed as props with jest

---
+++ src/components/mocktest-counter/Counter.tsx
@@ -0,0 +1,16 @@
+import { CounterProps } from "./Counter.types";
+
+export const Counter = (props: CounterProps) => {
+  return (
+    <div>
+      <h1>Counter</h1>
+      <p>{props.count}</p>
+      {props.handleIncrement && (
+        <button onClick={props.handleIncrement}>Increment</button>
+      )}
+      {props.handleDecrement && (
+        <button onClick={props.handleDecrement}>Decrement</button>
+      )}
+    </div>
+  );
+};
